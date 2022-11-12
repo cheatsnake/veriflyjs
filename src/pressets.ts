b254c055579bafd1da36591ae052d1db04b1927b
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2022-11-12 18:00:32 +0300

    added a new presset

--- src/pressets.ts
+++ src/pressets.ts
@@ -1,5 +1,6 @@
 import {
     INVALID_CAPITAL_LETTERS_AMOUNT,
+    INVALID_EMAIL,
     INVALID_NUMBERS_AMOUNT,
     INVALID_SPECIAL_SYMBOLS_AMOUNT,
 } from "./messages";
@@ -27,5 +28,16 @@ export const specialSymbolPresset = (minAmount = 1, maxAmount?: number): Presset
         patterns: [/\W/g],
         minAmount,
         errorMsg: () => INVALID_SPECIAL_SYMBOLS_AMOUNT({ minAmount, maxAmount }),
+    };
+};
+
+export const emailPresset = (): PressetOptions => {
+    return {
+        patterns: [
+            /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm,
+        ],
+        minAmount: 1,
+        maxAmount: 1,
+        errorMsg: INVALID_EMAIL,
     };
 };
